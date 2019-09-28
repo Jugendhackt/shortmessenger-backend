37114b8161071461221aa5a178899ee4bcdddcd4
Niklas Kreer <kreergit@posteo.de>
2019-09-28 12:12:37 +0200

    User authentication

--- src/index.php
+++ src/index.php
@@ -2,4 +2,28 @@
 
 header("Header: application/json")
 
+define("INVALID_CREDENTIALS_MSG", "Invalid credentials");
 
+$output = ["error" => true];
+
+# Check whether useraccount exists and is valid
+if(!empty($_GET["username"]) && !empty($_GET["password"])){
+	$name = basename($_GET["username"]);
+	# Does the user even exist?
+	if($file = file_get_contents("users/".$name.".json")){
+		$user = json_decode($file, true);
+		# Verify whether password is correct
+		if(password_verify($_GET["password"], $user["password"])){
+			# Yes! We know it's correct!!!!! nice ;)
+
+		} else {
+			$output["errormsg"] = INVALID_CREDENTIALS_MSG;
+		}
+	} else {
+		$output["errormsg"] = INVALID_CREDENTIALS_MSG;
+	}
+} else {
+	$output["errormsg"] = INVALID_CREDENTIALS_MSG;
+}
+
+echo json_encode($output);
